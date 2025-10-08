d4d1e1a5954bc7c3252221d01653ad945904e09a
chptcleo <chptcleo@hotmail.com>
2025-10-08 21:46:33 +0800

    Feature/qa 01 (#1)
    update ci and add readme

--- cypress/support/page-utils.ts
+++ cypress/support/page-utils.ts
@@ -2,6 +2,6 @@ export class PageUtils {
   // Open the Workspaces page
   openWorkspacesPage() {
     cy.viewport(1920, 1080);
-    cy.visit(Cypress.env("BASE_URL"));
+    cy.visit(Cypress.env("UI_URL"));
   }
 }
